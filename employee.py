7b4fe8fe212fecb9a672fa422baee8aeb6430d4d
Taaran Jain <taaranjain16@gmail.com>
2025-09-30 12:44:47 +0530

    Employee Sheet Changes 2

--- employee.py
+++ employee.py
@@ -3,7 +3,7 @@ from config import EMPLOYEE_SHEET_ID
 
 def load_employee_data():
     client = authorize()
-    sheet = client.open_by_key(EMPLOYEE_SHEET_ID).Dropdowns
+    sheet = client.open_by_key(EMPLOYEE_SHEET_ID).worksheet("Dropdowns")
     return {
         row[0].strip(): row[1].strip()
         for row in sheet.get_all_values()
